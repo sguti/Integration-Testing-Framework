a45119fec1144e1f7854a7af41bc339a01d5ffb8
sudip <sudipguti@gmail.com>
2018-10-29 23:27:59 +0530

    Prod build folder added
    Prod build folder added

---
+++ client/build/precache-manifest.676b2ca6b6663b8d9685b4292f6311e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2eb74c99f9d11d30305d",
+    "url": "/static/js/main.2eb74c99.chunk.js"
+  },
+  {
+    "revision": "158216f22a1bad924b41",
+    "url": "/static/js/1.158216f2.chunk.js"
+  },
+  {
+    "revision": "2eb74c99f9d11d30305d",
+    "url": "/static/css/main.14310e20.chunk.css"
+  },
+  {
+    "revision": "d8647b686e07dc0b01487912b516fbd9",
+    "url": "/index.html"
+  }
+];
